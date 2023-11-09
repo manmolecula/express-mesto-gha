6ea243708077f6de5a595d5d378290cacd5ca5c4
Anton Kuchev <kuchevanton@bk.ru>
2023-11-09 23:13:54 +0300

    2 try

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const {getCards, createCard, deleteCardById, likeCard, dislikeCard} = require('../controllers/cards');
+const {
+  getCards, createCard, deleteCardById, likeCard, dislikeCard,
+} = require('../controllers/cards');
 
 router.get('/', getCards);
 router.post('/', createCard);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const {getUsers, getUserById, createUser, updateProfile, updateAvatar} = require('../controllers/users');
+const {
+  getUsers, getUserById, createUser, updateProfile, updateAvatar,
+} = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
